a95c7ca6a0bcf63ea8a2c094a7ffeaf978b158c6
dottorrao <marco.ruggeri@gmail.com>
2020-04-01 14:58:02 +0200

    Last changes

--- covid19-situation.py
+++ covid19-situation.py
@@ -16,7 +16,7 @@ except AttributeError: raise RuntimeError("Use IDLE")
 
 ##############################################################################################################
 #GLOBAL VARS
-dayM = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30] #marzo
+dayM = [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31] #marzo
 dayA = [] #aprile
 date=[]
 casi=0
@@ -39,7 +39,7 @@ def plotGraph(x,y,xLabel,ylabel,title):
 # ITALIA - CASI PER TAMPONI
 ##############################################################################################################
 casiTampArIt=[]
-casiArIt=[]
+casiTampArItLog=[]
 
 def elabCasiTampArIt(tempS,url):
     data = urllib.request.urlopen(url+tempS + '.csv')
@@ -60,7 +60,7 @@ def calcCasiTampone(day,casi,tamponi,mese):
         month = "Arile" 
     ct = float( (format(round(casi/tamponi,5),'.5f')) )
     casiTampArIt.append (ct)
-    casiArIt.append( math.log10(int(dat[0])) )
+    casiTampArItLog.append( math.log10(int(dat[0])) )
     date.append(datetime(year=2020, month=mese, day=d))
     print (day + ' ' + month + ' : ', end = '')
     print (str(ct), end = '')
@@ -78,7 +78,7 @@ for d in dayA:
     calcCasiTampone(str(d),dat[0],dat[1],4)
 
 plotGraph(date,casiTampArIt,'Giorni','Casi per tamponi','Italia - Casi per tamponi')
-plotGraph(date,casiArIt,'Giorni','Casi','Italia - Casi scala log')
+plotGraph(date,casiTampArItLog,'Giorni','Casi per tamponi','Italia - Casi per tamponi (scala log.)')
 
 ##############################################################################################################
 #ITALIA - TERAPIA INTENSIVA
@@ -100,7 +100,8 @@ def calcCasiTerInITA(day,terIn,mese):
         month = "Marzo"
     elif mese == 4:
         month = "Arile" 
-    terInIt.append (terIn)
+    terInArrIt.append (terIn)
+    terInArrItLog.append ( math.log10(int(terIn)) )
     diff = ( terIn - terInOLd )
     print (day + ' ' + month + ' : ' + str(terIn), end = '')
     print (" - Differenza giorno prec: " + str(diff) )
@@ -108,7 +109,8 @@ def calcCasiTerInITA(day,terIn,mese):
 
 color.write ("ITALIA - INCREMENTO RICOVERATI TERAPIA INTENSIVA \n","STRING" )
 terInOLd = 0
-terInIt=[]
+terInArrIt=[]
+terInArrItLog=[]
 for d in dayM:
     dat = elabCasiTerInITA(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-202003')
     calcCasiTerInITA(str(d),dat,3)
@@ -117,15 +119,16 @@ for d in dayA:
     dat = elabCasiTerInITA(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-202004s')
     calcCasiTerInITA(str(d),dat,4)
     
-plotGraph(date,terInIt,'Giorni','Terapie Intensive','Italia - Ricoveri terapia intensiva')
+plotGraph(date,terInArrIt,'Giorni','Terapie Intensive','Italia - Ricoveri terapia intensiva')
+plotGraph(date,terInArrItLog,'Giorni','Terapie Intensive','Italia - Ricoveri terapia intensiva (scala log.)')
 
 
 ##############################################################################################################
 # TOSCANA - CASI PER TAMPONI
 ##############################################################################################################
 color.write ("TOSCANA- POSITIVI PER TAMPONI EFFETTUATI \n","STRING" )
-casiArTampTo=[]
-casiArTo=[]
+casiTampArTo=[]
+casiTampArToLog=[]
 
 def elabCasiTampArTO(tempS,url):
     data = urllib.request.urlopen(url + tempS + '.csv')
@@ -146,8 +149,8 @@ def calcCasiTampArTO(day,casi,tamponi,mese):
     elif mese == 4:
         month = "Arile"
     ct = float( (format(round(casi/tamponi,5),'.5f')) )
-    casiArTampTo.append (ct)
-    casiArTo.append( math.log10(int(casi)) )
+    casiTampArTo.append (ct)
+    casiTampArToLog.append( math.log10(int(casi)) )
     print (day + ' ' + month + ' : ', end = '')
     print (str(ct), end = '')
     print (" - casi rilevati: " + str(casi), end = '')
@@ -161,8 +164,8 @@ for d in dayA:
     dat = elabCasiTampArTO(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202004')
     calcCasiTampArTO(str(d),dat[0],dat[1],3)
    
-plotGraph(date,casiArTampTo,'Giorni','Casi per tamponi','Toscana - Casi per tamponi')
-plotGraph(date,casiArTo,'Giorni','Casi','Toscana - Casi scala log')
+plotGraph(date,casiTampArTo,'Giorni','Casi per tamponi','Toscana - Casi per tamponi')
+plotGraph(date,casiTampArToLog,'Giorni','Casi per tamponi','Toscana - Casi per tamponi (scala log.)')
 
 
 ##############################################################################################################
@@ -187,6 +190,7 @@ def calcCasiTerInTO(day,terIn,mese):
     elif mese == 4:
         month = "Arile" 
     terInTo.append (terIn)
+    terInToLog.append ( math.log10(int(terIn)) )
     diff = ( terIn - terInOLd )
     print (day + ' ' + month + ' : ' + str(terIn), end = '')
     print (" - Differenza giorno prec: " + str(diff) )
@@ -196,6 +200,7 @@ color.write ("TOSCANA - INCREMENTO RICOVERATI TERAPIA INTENSIVA \n","STRING" )
 
 terInOLd = 0
 terInTo=[]
+terInToLog=[]
 for d in dayM:
     dat = elabCasiTerInTO(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202003')
     calcCasiTerInTO(str(d),dat,3)
@@ -204,4 +209,55 @@ for d in dayA:
     dat = calcCasiTerInTO(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni-202004')
     calcCasiTerInTO(str(d),dat,4)
 
-plotGraph(date,terInTo,'Giorni','Terapie Intensive','Italia - Ricoveri terapia intensiva')
+plotGraph(date,terInTo,'Giorni','Terapie Intensive','Toscana - Ricoveri terapia intensiva')
+plotGraph(date,terInToLog,'Giorni','Terapie Intensive','Toscana - Ricoveri terapia intensiva - Scala log')
+
+
+##############################################################################################################
+# PRATO - CASI
+##############################################################################################################
+casPo=[]
+casPoLog=[]
+
+def elabCasiPO(tempS,url):
+    data = urllib.request.urlopen(url+tempS + '.csv')
+    myData = data.read()
+    mystr = myData.decode("utf8")
+    data.close()
+    mySplit = mystr.splitlines()
+    poRowSplit = mySplit[111].split(',')
+    if poRowSplit[9] == '':
+        casi = 0
+    else:
+        casi = int(poRowSplit[9])
+    return casi
+
+def calcCasiPO(day,casi,mese):
+    month = ""
+    if mese == 3:
+        month = "Marzo"
+    elif mese == 4:
+        month = "Arile" 
+
+    casPo.append (casi)
+
+    if casi == 0:
+        casPoLog.append( 0 )
+    else:
+        casPoLog.append( math.log10(int(casi)) )
+    
+    print (day + ' ' + month + ' : ', end = '')
+    print (" - casi rilevati:" + str(casi) )
+
+color.write ("PRATO - CASI \n","STRING")
+
+for d in dayM:
+    dat = elabCasiPO(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-202003')
+    calcCasiPO(str(d),dat,3)
+    
+for d in dayA:
+    dat = elabCasiPO(str(d),'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-province/dpc-covid19-ita-province-202004')
+    calcCasiPO(str(d),dat,4)
+
+plotGraph(date,casPo,'Giorni','Casi','Prato - Casi')
+plotGraph(date,casPoLog,'Giorni','Casi','Prato - Casi (scala log.)')
